49bfd7627e3ad98c9c78c57fc10fc5e6d5c345f8
Evan Trujillo <evantrujillo30@gmail.com>
2023-12-12 15:58:57 +0900

    Add design system (#12)
    * Add custom fonts
    
    * Setup global styles
    
    * Add assets
    
    * refactor directory structure
    
    * Add shadcn ui components
    
    * Add configuration for shadcn ui
    
    * Update tailwind
    
    * Update global style colors
    * Update tailwind config to work with shadcn ui components
    
    * Run prettier
    
    * Remove windows added files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,49 @@
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  darkMode: ['class'],
+  content: ['app/src/**/*.{ts,tsx}', 'app/src/components/**/*.{ts,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'app-background': 'var(--app-background)',
+        'on-light-background': 'var(--on-light-background)',
+        'on-dark-background': 'var(--on-dark-background)',
+        'settings-background': 'var(--settings-background)',
+        'primary-dark': 'var(--primary-dark)',
+        'secondary-peach': 'var(--secondary-peach)',
+        'secondary-aqua': 'var(--secondary-aqua)',
+        'secondary-purple': 'var(--secondary-purple)',
+      },
+      fontFamily: {
+        kumbh: ['var(--font-kumbh-sans)', ...fontFamily.sans],
+        space: ['var(--font-space-mono)', ...fontFamily.sans],
+        roboto: ['var(--font-roboto-slab)', ...fontFamily.sans],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0px' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0px' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
