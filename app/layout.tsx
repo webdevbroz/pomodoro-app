e2a7276f17bbb9cc77c053a428911f54d3705868
Evan <evantrujillo30@gmail.com>
2023-12-18 00:09:43 +0900

    WIP: define layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${kumbh.variable} ${roboto.variable} ${space.variable}`}>{children}</body>
+      <body className={`${kumbh.variable} ${roboto.variable} ${space.variable}`}>
+        <div className="flex min-h-screen flex-col">
+          <main className="flex-1">
+            <div className="container max-w-[1440px] pb-8 pt-6">{children}</div>
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Placeholder from './src/components/placeholder';
 
 export default function Home() {
-  return <Placeholder text="Click me" />;
+  return (
+    <div className="flex max-h-[819px] max-w-md flex-col">
+     <div className="h-[200px] w-full border-4 border-white"></div>
+     <div className="h-[200px] w-full border-4 border-white"></div>
+     <div className="h-[500px] w-full border-4 border-white"></div>
+     <div className="h-[200px] w-full border-4 border-white"></div>
+    </div>
+  )
 }
