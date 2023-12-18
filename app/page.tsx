72dc0625e0dd89c57336ca7fcb959412df9e4c8a
Evan Trujillo <evantrujillo30@gmail.com>
2023-12-18 16:02:30 +0900

    Refactor test directory (#19)
    * Refactor __tests__ directory
    
    * Remove Placeholder component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Placeholder from './src/components/placeholder';
+
 
 export default function Home() {
-  return <Placeholder text="Click me" />;
+
 }
--- app/src/components/placeholder.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from '@/src/components/ui/button';
-
-export default function Placeholder({ text }: { text?: string }) {
-  return <Button className="text-on-light-background">{text}</Button>;
-}
