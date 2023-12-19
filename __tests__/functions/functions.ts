43a8c900834b4f50e5fe32424ec9d831befad33b
Robert.K <therobertkirkman@gmail.com>
2023-12-19 15:57:47 +0900

    Add component integration tests

---
+++ __tests__/functions/functions.ts
@@ -0,0 +1,48 @@
+import { cleanPercentage } from '@/src/components/circular-progress';
+
+describe("cleanPercentage", () => {
+  it("should return a number if the input is a within the values of 0-100", () => {
+    // When input is a number
+    expect(cleanPercentage(50)).toBe(50);
+
+    // When input is a string
+    expect(cleanPercentage('30')).toBe(30);
+  })
+
+  it("should return 0 for negative values", () => {
+    // When given a negative numerical value
+    expect(cleanPercentage(-15)).toBe(0);
+
+    // When given a negative alphabetic value
+    expect(cleanPercentage("-15")).toBe(0);
+  })
+
+  it("should return 100 for values greater than 100", () => {
+    // When given a negative numerical value
+    expect(cleanPercentage(110)).toBe(100);
+
+    // When given a negative alphabetic value
+    expect(cleanPercentage("110")).toBe(100);
+  })
+
+  it("should return 0 for NaN values", () => {
+    expect(cleanPercentage("abc")).toBe(0);
+  })
+
+  it("testing edge cases", () => {
+    // Return 0 when given 0
+    expect(cleanPercentage(0)).toBe(0);
+
+    // Return 100 when given 100
+    expect(cleanPercentage(100)).toBe(100);
+
+    // Return 100 when given a super duper pumper to the maxMaxMAX number
+    expect(cleanPercentage(Number.MAX_VALUE)).toBe(100);
+  })
+
+  it("should return 0 for special numeric values", () => {
+    expect(cleanPercentage(NaN)).toBe(0);
+    expect(cleanPercentage(Infinity)).toBe(0);
+    expect(cleanPercentage(-Infinity)).toBe(0);
+  })
+})
--- app/src/components/circular-progress.tsx
+++ app/src/components/circular-progress.tsx
@@ -6,38 +6,52 @@ export const cleanPercentage = (percentage: number | string): number => {
   return isNegativeOrNaN ? 0 : isTooHigh ? 100 : +percentage;
 };
 
-const ProgressCircle = ({ colour, percentage }: { colour: string; percentage: number | string }): ReactElement => {
+export const ProgressCircle = ({
+  colour,
+  percentage,
+}: {
+  colour: string;
+  percentage: number | string;
+}): ReactElement => {
   // An SVG circle with stroke-width renders two concentric circles (or a doughnut like image)
   // with its internal radius as r - ½ the stroke width and its external radius as r + ½ stroke-width.\
   // For a circle with d=400, r should be (r - strokeWidth / 2)
   const r = 197.5; // (r * 2) + strokeWidth needs to equal the size of the intended circle - this this case 410
   const circ = 2 * Math.PI * r;
-  const strokePct = ((100 - +percentage) * circ) / 100; // where stroke will start, e.g. from 15% to 100%.
+  const strokePercentage = ((100 - +percentage) * circ) / 100; // where stroke will start, e.g. from 15% to 100%.
   return (
     <circle
+      data-testid={`progress-circle-${colour}-test`}
       r={r}
       cx={-5}
       // cy={-105} // Can remove this and inclde in transform translate attribute
       fill="transparent"
-      stroke={strokePct !== circ ? colour : ''} // remove colour as 0% sets full circumference
+      stroke={strokePercentage !== circ ? colour : ''} // remove colour as 0% sets full circumference
       strokeWidth={15}
       strokeDasharray={circ}
-      strokeDashoffset={percentage ? strokePct : 0}
+      strokeDashoffset={percentage ? strokePercentage : 0}
       transform="scale(1, -1) translate(0, -205)" // this flips the progress bar to empty clockwise
     ></circle>
   );
 };
 
-const StringToSpans = ({ text, xvalue }: { text: string; xvalue: number }): ReactElement => {
+export const StringToTextElement = ({
+  text,
+  xvalue,
+}: {
+  text: string;
+  xvalue: number;
+}): ReactElement => {
   return (
     <>
       {text.split('').map((char, index) => (
         <text
+          data-testid={`strings-to-text-element-test-${index}`}
           className="fill-on-dark-background text-8xl font-bold"
           dominantBaseline="central"
           textAnchor="middle"
           key={index}
-          x={`${xvalue + index * 13}` + '%'}
+          x={`${xvalue + index * 13}%`}
           y="50%"
         >
           {char}
@@ -47,17 +61,23 @@ const StringToSpans = ({ text, xvalue }: { text: string; xvalue: number }): Reac
   );
 };
 
-const RenderTimerText = ({ minutes, seconds }: { minutes: string; seconds: string }): ReactElement => {
+export const RenderTimerText = ({
+  minutes,
+  seconds,
+}: {
+  minutes: string;
+  seconds: string;
+}): ReactElement => {
   return (
     // https://developer.mozilla.org/en-US/docs/Web/SVG/Element/text
     // 21.66 is the width of the semicolon html element
-    <>
-      <StringToSpans xvalue={27} text={minutes} />
+    <g data-testid="render-timer-text-test">
+      <StringToTextElement xvalue={27} text={minutes} />
       <text className="fill-on-dark-background text-8xl" x={205 - 21.66 / 2} y={205 + 21.66}>
         :
       </text>
-      <StringToSpans xvalue={60} text={seconds} />
-    </>
+      <StringToTextElement xvalue={60} text={seconds} />
+    </g>
   );
 };
 
@@ -74,10 +94,10 @@ export const CircularProgress = ({
 }): ReactElement => {
   const pct = cleanPercentage(percentage);
   return (
-    <svg className="flex " width={410} height={410}>
+    <svg data-testid="circular-progress-test" className="flex" width={410} height={410}>
       <g transform={`rotate(-90 ${'100 100'})`}>
-        <ProgressCircle colour="lightgrey" percentage={percentage} />
-        <ProgressCircle colour={colour} percentage={pct} />
+        <ProgressCircle data-testid="progress-circle-lower" colour="lightgrey" percentage={percentage} />
+        <ProgressCircle data-testid="progress-circle-upper" colour={colour} percentage={pct} />
       </g>
       <RenderTimerText minutes={minutes} seconds={seconds} />
     </svg>
