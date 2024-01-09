e54589bd5c28c7d5d3d4fe0024b00806d7427c1c
Evan Trujillo <evantrujillo30@gmail.com>
2024-01-09 16:33:33 +0900

    Refactor: Split components
    * Split Radio item component
    * Create explicit type for values passed

--- src/components/timer-transition.tsx
+++ src/components/timer-transition.tsx
@@ -4,34 +4,25 @@ import { ReactElement, useState } from 'react';
 import { Label } from './ui/label';
 import { RadioGroup, RadioGroupItem } from './ui/radio-group';
 
+type RadioValue = 'pomodoro' | 'short' | 'long';
+
 export default function TimerTransition(): ReactElement {
-  const [selected, setSelected] = useState<string>('pomodoro');
+  const [selected, setSelected] = useState<RadioValue>('pomodoro');
 
-  ('bg-secondary-peach px-[23px] py-[16px]');
-  console.log(selected);
+  const RadioItems = ({ selected }: { selected: RadioValue }): ReactElement => {
+    function setSelectedStyles<T extends RadioValue>(selected: T, value: T): string {
+      return selected === value ? 'bg-secondary-peach' : 'text-gray-500 hover:text-on-dark-background';
+    }
 
-  // function setClassName(selected: string) {
-  //   switch(selected) {
-  //     case 'pomodoro':
-  //       return 'bg-secondary-peach px-[23px] py-[16px]'
-  //     case 'short':
-  //       return
-  //   }
-  // }
-
-  return (
-    <div className="z-10 flex h-[3.938rem] w-[20.438rem] items-center justify-center rounded-full bg-primary-dark md:w-[23.313rem]">
-      <RadioGroup
-        onValueChange={(value) => setSelected(value)}
-        className="flex w-full justify-center gap-0"
-        defaultValue="pomodoro"
-      >
+    return (
+      <>
         <div className="flex w-[106px] items-center md:w-[120px]">
           <RadioGroupItem className="w-0 opacity-0" value="pomodoro" id="radioPomodoro" />
           <Label
-            className={`inline-block cursor-pointer rounded-full px-[23px] py-[18px] text-xs font-bold md:px-[26px] md:text-sm ${
-              selected === 'pomodoro' ? 'bg-secondary-peach' : 'text-gray-500 hover:text-on-dark-background'
-            }`}
+            className={`inline-block cursor-pointer rounded-full px-[23px] py-[18px] text-xs font-bold md:px-[26px] md:text-sm ${setSelectedStyles(
+              selected,
+              'pomodoro'
+            )}`}
             htmlFor="radioPomodoro"
           >
             pomodoro
@@ -40,9 +31,10 @@ export default function TimerTransition(): ReactElement {
         <div className="flex w-[106px] items-center md:w-[120px]">
           <RadioGroupItem className="w-0 opacity-0" value="short" id="radioShort" />
           <Label
-            className={`inline-block cursor-pointer whitespace-nowrap rounded-full px-[20px] py-[16px] text-xs font-bold md:px-[22px] md:text-sm ${
-              selected === 'short' ? 'bg-secondary-peach' : 'text-gray-500 hover:text-on-dark-background'
-            }`}
+            className={`inline-block cursor-pointer whitespace-nowrap rounded-full px-[20px] py-[16px] text-xs font-bold md:px-[22px] md:text-sm ${setSelectedStyles(
+              selected,
+              'short'
+            )}`}
             htmlFor="radioShort"
           >
             short break
@@ -51,14 +43,27 @@ export default function TimerTransition(): ReactElement {
         <div className="flex w-[106px] items-center md:w-[120px]">
           <RadioGroupItem className="w-0 opacity-0" value="long" id="radioLong" />
           <Label
-            className={`inline-block cursor-pointer whitespace-nowrap rounded-full px-[21px] py-[16px] text-xs font-bold md:px-[25px] md:text-sm ${
-              selected === 'long' ? 'bg-secondary-peach' : 'text-gray-500 hover:text-on-dark-background'
-            }`}
+            className={`inline-block cursor-pointer whitespace-nowrap rounded-full px-[21px] py-[16px] text-xs font-bold md:px-[25px] md:text-sm ${setSelectedStyles(
+              selected,
+              'long'
+            )}`}
             htmlFor="radioLong"
           >
             long break
           </Label>
         </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="z-10 flex h-[3.938rem] w-[20.438rem] items-center justify-center rounded-full bg-primary-dark md:w-[23.313rem]">
+      <RadioGroup
+        onValueChange={(value: RadioValue) => setSelected(value)}
+        className="flex w-full justify-center gap-0"
+        defaultValue="pomodoro"
+      >
+        <RadioItems selected={selected} />
       </RadioGroup>
     </div>
   );
