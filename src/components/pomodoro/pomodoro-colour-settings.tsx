2f08643a88c7ad1302206b30ad386d7f1d64653e
Robert.K <therobertkirkman@gmail.com>
2024-01-15 18:18:51 +0900

    Formatting

--- src/components/pomodoro/pomodoro-colour-settings.tsx
+++ src/components/pomodoro/pomodoro-colour-settings.tsx
@@ -1,6 +1,6 @@
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
-import { Label } from '@/components/ui/label';
 import { ReactElement, useState } from 'react';
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Colours } from '@/lib/colours';
 
 interface PomodoroColourSettingsProps {
@@ -15,12 +15,25 @@ export default function PomodoroColourSettings({ onColourChange }: PomodoroColou
     onColourChange(value);
   };
   return (
-    <div>
-      <RadioGroup className="flex" onValueChange={handleValueChange}>
-        <RadioGroupItem className='dark:bg-secondary-peach h-[40px] w-[40px] ring-offset-red-950' value="#f87070" id="peach" checked={selectedColourValue === Colours.SecondaryPeach} />
-        <RadioGroupItem className='dark:bg-secondary-aqua h-[40px] w-[40px]' value="#70f3f8" id="aqua" checked={selectedColourValue === Colours.secondaryAqua} />
-        <RadioGroupItem className='dark:bg-secondary-purple h-[40px] w-[40px]' value="#d881f8" id="purple" checked={selectedColourValue === Colours.secondaryPurple} />
-      </RadioGroup>
-    </div>
+    <RadioGroup className="flex justify-center" onValueChange={handleValueChange}>
+      <RadioGroupItem
+        className="dark:bg-secondary-peach h-[40px] w-[40px] ring-offset-red-950"
+        value="#f87070"
+        id="peach"
+        checked={selectedColourValue === Colours.SecondaryPeach}
+      />
+      <RadioGroupItem
+        className="dark:bg-secondary-aqua h-[40px] w-[40px]"
+        value="#70f3f8"
+        id="aqua"
+        checked={selectedColourValue === Colours.secondaryAqua}
+      />
+      <RadioGroupItem
+        className="dark:bg-secondary-purple h-[40px] w-[40px]"
+        value="#d881f8"
+        id="purple"
+        checked={selectedColourValue === Colours.secondaryPurple}
+      />
+    </RadioGroup>
   );
 }
