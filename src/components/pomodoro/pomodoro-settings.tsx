b317c24964ade879ab541e78c162dc82ca96f74e
Evan Trujillo <evantrujillo30@gmail.com>
2024-01-23 16:36:16 +0900

    Add pomodoro timer setting
    * Fixed tailwind class order

--- src/components/pomodoro/pomodoro-settings.tsx
+++ src/components/pomodoro/pomodoro-settings.tsx
@@ -5,6 +5,7 @@ import { ReactElement, useState } from 'react';
 import Image from 'next/image';
 import PomodoroColourSettings from '@/components/pomodoro/pomodoro-colour-settings';
 import PomodoroFontSettings from '@/components/pomodoro/pomodoro-font-settings';
+import PomodoroTimerSettings from './pomodoro-timer-settings';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { pomodoroSettingsSlice } from '@/lib/redux/slices/pomodoroSettingsSlice';
@@ -40,10 +41,11 @@ export default function PomodoroSettings(): ReactElement {
       <DialogTrigger className="">
         <Image src="/icon-settings.svg" width={28} height={28} alt="settings" className="cursor-pointer" />
       </DialogTrigger>
-      <DialogContent className="flex h-[575px] w-[327px]	flex-col justify-between dark:bg-settings-background md:h-[540px] md:w-[490px]">
+      <DialogContent className="flex h-[575px] w-[327px] flex-col justify-between md:h-[540px] md:w-[490px] dark:bg-settings-background">
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
         </DialogHeader>
+        <PomodoroTimerSettings />
         <PomodoroColourSettings onColourChange={handleColourChange} />
         <PomodoroFontSettings onFontChange={handleFontChange} />
         <Button onClick={handleApplyNewSettings}>Apply</Button>
---
+++ src/components/pomodoro/pomodoro-timer-settings.tsx
@@ -0,0 +1,93 @@
+import { ChangeEvent, useState } from 'react';
+import { Colours } from '@/lib/colours';
+import { ReduxState, useSelector } from '@/lib/redux/store';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+
+interface PomodoroTimerSettingsProps {
+  onTimerChange: (timer: string) => void;
+}
+
+interface PomodoroTimerData {
+  pomodoro: number;
+  shortBreak: number;
+  longBreak: number;
+}
+
+export default function PomodoroTimerSettings({ onTimerChange }: PomodoroTimerSettingsProps) {
+  const { colour } = useSelector((state: ReduxState) => state.pomodoroSettings);
+  const [selectedTime, setSelectedTime] = useState<PomodoroTimerData>({ pomodoro: 25, shortBreak: 5, longBreak: 15 });
+  const inputRules = {
+    min: 5,
+    max: 60,
+    step: 5
+  };
+
+  function setRingOffsetColor(colour: string) {
+    switch (colour) {
+      case Colours.SecondaryAqua:
+        return 'focus:border-secondary-aqua';
+      case Colours.SecondaryPurple:
+        return 'focus:border-secondary-purple';
+    }
+    return 'focus:border-secondary-peach';
+  }
+
+  const handleTimeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setSelectedTime((prevSelectedTime) => ({
+      ...prevSelectedTime,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Label className=" pb-3 text-center text-xs font-bold uppercase tracking-[0.4em] md:self-start">
+        Time (Minutes)
+      </Label>
+      <div className="flex flex-col items-center gap-2 md:flex-row">
+        <div className="flex w-full items-center justify-between md:flex-col md:items-start md:gap-2">
+          <Label className="text-xs font-bold text-gray-400">pomodoro</Label>
+          <Input
+            name="pomodoro"
+            className={`font-bold ${setRingOffsetColor(colour)}`}
+            type="number"
+            onChange={handleTimeChange}
+            value={selectedTime.pomodoro}
+            min={inputRules.min}
+            max={inputRules.max}
+            step={inputRules.step}
+          />
+        </div>
+        <div className="flex w-full items-center justify-between md:flex-col md:items-start md:gap-2">
+          <Label className="text-xs font-bold text-gray-400">short break</Label>
+          <Input
+            name="shortBreak"
+            className={`font-bold ${setRingOffsetColor(colour)}`}
+            type="number"
+            onChange={handleTimeChange}
+            value={selectedTime.shortBreak}
+            min={inputRules.min}
+            max={inputRules.max}
+            step={inputRules.step}
+          />
+        </div>
+        <div className="flex w-full items-center justify-between md:flex-col md:items-start md:gap-2">
+          <Label className="text-xs font-bold text-gray-400">long break</Label>
+          <Input
+            name="longBreak"
+            className={`font-bold ${setRingOffsetColor(colour)}`}
+            type="number"
+            onChange={handleTimeChange}
+            value={selectedTime.longBreak}
+            min={inputRules.min}
+            max={inputRules.max}
+            step={inputRules.step}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
